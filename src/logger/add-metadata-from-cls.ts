d6dfc89a644a478515046d3d96e5ff27b14a047f
Edgar Pogosyan <edgar.pogosyan.a@gmail.com>
2023-05-16 01:51:46 +0300

    feat: clear logs

--- src/logger/add-metadata-from-cls.ts
+++ src/logger/add-metadata-from-cls.ts
@@ -1,9 +1,15 @@
 import winston from 'winston';
 import { Namespace } from 'cls-hooked';
+import _ from 'lodash';
+import { ContextMessageUpdate } from 'telegraf';
 
-export const addMetadataFromCls: TAddMetadataFromClsFormatWrap = winston.format(
-  (info, opts: IAddMetadataFromClsOptions) => {
-    info[opts.metadataProp] = opts.clsNs.get(opts.clsProp);
+export const addBotContextFromCls: TAddBotContextFromClsWrap = winston.format(
+  (info, opts: IAddBotContextFromCls) => {
+    const botContext = opts.clsNs.get('bot_context') as ContextMessageUpdate;
+    // When ContextMessageUpdate has private 'tg' property
+    // which appears in logs, so we remove it here.
+    // 'botInfo' is just redundant, so we just remove it.
+    info['bot_context'] = _.omit(botContext, ['tg', 'botInfo']);
     return info;
   },
 );
@@ -19,12 +25,10 @@ export type TWinstonFormat = ReturnType<TWinstonFormatWrap>;
  * after we got type Format we can create custom
  * FormatWrap type, with strongly typed opts argument
  */
-export type TAddMetadataFromClsFormatWrap = (
-  opts: IAddMetadataFromClsOptions,
+export type TAddBotContextFromClsWrap = (
+  opts: IAddBotContextFromCls,
 ) => TWinstonFormat;
 
-export interface IAddMetadataFromClsOptions {
+export interface IAddBotContextFromCls {
   clsNs: Namespace;
-  clsProp: string;
-  metadataProp: string;
 }
